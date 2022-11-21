522a5ece13a295e283bcf66e14357c8742148114
Suresh-Kumar-Lohar <sureshlohar468@gmail.com>
2022-11-21 12:03:13 +0530

    expense-tracker task-13

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,8 +1,17 @@
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import classes from './Counter.module.css'
 
 const Counter = () => {
+  const dispatch = useDispatch()
   const counter = useSelector((state) => state.counter)
+
+  const incrementHandlerBy5 = () => {
+    dispatch({ type: 'incrementBy5' })
+  }
+
+  const decrementHandlerBy5 = () => {
+    dispatch({ type: 'decrementBy5' })
+  }
 
   const toggleCounterHandler = () => {}
 
@@ -10,6 +19,14 @@ const Counter = () => {
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
+      {/* <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div> */}
+      <div>
+        <button onClick={incrementHandlerBy5}>Increment By 5</button>
+        <button onClick={decrementHandlerBy5}>Decrement By 5</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   )
